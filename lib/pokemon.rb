9c2efdc5971873771f749d71d5a4bec4c25aa55e
Margot Konig <margot.konig@blackrock.com>
2019-06-13 05:28:08 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,24 +1,29 @@
 class Pokemon
-  attr_accessor :name, :type, :db, :id, :hp
-
-  def initialize(keywords)
-
+  attr_accessor :name, :type, :db, :hp
+  attr_reader :id
+  
+  def initialize(id:, name:, type:, db:, hp: nil)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @hp = hp
   end
-
+  
   def self.save(name, type, db)
-    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name,type)
   end
-
+  
   def self.find(num, db)
-    pokemon = db.execute("SELECT * FROM pokemon WHERE id=?", [num])
-    new_pokemon = self.new(pokemon)
-    new_pokemon.id = pokemon[0][0]
-    new_pokemon.name = pokemon[0][1]
-    new_pokemon.type = pokemon[0][2]
-    new_pokemon.hp = pokemon[0][3]
+    pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", [num])
+    id = pokemon[0][0]
+    name = pokemon[0][1]
+    type = pokemon[0][2]
+    hp = pokemon[0][3]
+    new_pokemon = self.new(id: id, name: name, type: type, db: db, hp: hp)
     return new_pokemon
   end
-
+  
   def alter_hp(num, db)
     db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", [num], [self.id])
     self.hp = num
