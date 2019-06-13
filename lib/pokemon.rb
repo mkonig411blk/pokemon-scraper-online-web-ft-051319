c2c136498135ae47f4fafab2479d1be4a0828899
Margot Konig <margot.konig@blackrock.com>
2019-06-13 05:00:57 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -16,7 +16,7 @@ class Pokemon
     SQL
     DB[:conn].execute(sql, self.name, self.type, self.db)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
-end
+  end
   
   def find 
   end 
